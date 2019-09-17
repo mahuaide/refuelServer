e8a6a1cb376ddc94e47e610fc1777d6d1a5a564f
mahd <mahd@asiainfo.com>
2019-09-17 10:41:42 +0800

    更新

---
+++ config/gitlabServer.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2019/9/16.
+ */
+/**
+ * gitlab服务启动地址和端口,PRIVATE-TOKEN从gitlab的account中查询
+ */
+module.exports = {
+    hostname: '10.1.241.24',
+    port: 80,
+    headers: {
+        'PRIVATE-TOKEN': 'Tg3zu5L9oxWTg6QCdSjj',
+    }
+}
