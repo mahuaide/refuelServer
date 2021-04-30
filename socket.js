5cd6a9ac91b4952baf116234cd5bf51f1b3c85ce
mahd1 <mhd_001@sina.com>
2021-04-30 18:00:36 +0800

    socket read file

--- socket.js
+++ socket.js
@@ -2,51 +2,50 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var fs = require('fs');
+const {
+  callbackify
+} = require('util');
 var http = require('http').Server(app);
-
+let fileName = "/log/access.log"
 var io = require('socket.io')(http, {
-    path:'/socket.io',
-    pingInterval: 1000,
-    pingTimeout: 5000,
-    cookie: true
+  path: '/socket.io',
+  pingInterval: 1000,
+  pingTimeout: 5000,
+  cookie: true
+});
+
+function sendLog(socket) {
+  fs.readFile(path.join(__dirname, fileName), 'utf-8', (err, data) => {
+    socket.emit("sendLog", decodeURIComponent(data))
+  })
+}
+
+io.on("connection", function (socket) {
+  console.log("connection")
+  // 接收到客户端请求，查询日志
+  socket.on("getLog", function (query) {
+    // 先全量发送一边
+    sendLog(socket);
+    // 如果日志有变化,间隔10S
+    fs.watchFile(path.join(__dirname, fileName),[false,true,1000], (cur, prv) => {
+      // 修改时间不一致
+      if (cur.mtime != prv.mtime) {
+        // 发送新得日志全量信息
+        sendLog(socket);
+      }
+    })
+  })
+  socket.on("disconnect", (reason) => {
+    console.log("disconnect")
+  });
 });
 
 
-io.on("connection", function (socket) {
-    //第一次握手时，取得项目ID
-    console.log(socket.handshake.query.projectId);
-    //通道ID
-    let socketid = socket.id;
-    //监听客户端发起查询项目状态请求，query中放projectId
-    socket.on("getLog", function (query) {
-      let lastData = "";
-      //查询请求，从参数中获取项目ID
-      setInterval(function () {
-        //1秒轮询数据库或者文件
-        console.log(new Date().toTimeString())
-        let fileName = "/log/access.log"
-        fs.readFile(path.join(__dirname, fileName), 'utf-8', (err, data) => {
-          let dataNew = data;
-          if (lastData !== dataNew) {
-            lastData = dataNew;
-            //广播方式
-            socket.emit("sendLog",lastData)
-            //点对点方式
-            // if (io.sockets.connected[socketid]) {
-            //   io.sockets.connected[socketid].emit("sendLog", lastData))
-            // }else{
-            //   socket.disconnect(true)
-            // }
-          }
-        })
-      }, 1000)
-    })
-  });
-
-http.listen(3003,"127.0.0.1",(err) => {
-    if (err) {
-        console.log(err);
-        return
-    }
-    console.log('websocket Listening at http://127.0.0.1:3003')
+http.listen(3003, "127.0.0.1", (err) => {
+  if (err) {
+    console.log(err);
+    return
+  }
+  console.log('websocket Listening at http://127.0.0.1:3003')
 })
+
